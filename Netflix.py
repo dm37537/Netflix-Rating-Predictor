1027f42c092114183f988808c4650e620ed6e2f2
Da Meng <dameng@utexas.edu>
2015-02-12 16:42:37 -0600

    read cache

--- Netflix.py
+++ Netflix.py
@@ -1,17 +1,34 @@
 import sys
+import json 
 
 def netflix_read (s) :
     """
-    read two ints
-    s a string
-    return a list of two ints, representing the beginning and end of a range, [i, j]
     """
+
     if len(s) < 7:
     	a = s.split(':')
     	return [str(a[0]), str(a[1]), ""]
     else:
 	    a = s.split(',')
 	    return [str(a[0]), str(a[1]), str(a[2])]
+
+def probe_read(f):
+    
+    l = {}
+    movie_id = ''
+    #users = []
+
+    for s in f:
+        #print (s[-2])
+        if s[-2] == ':':
+            movie_id = s[0:-2]
+            users =[]
+            l[movie_id] = users
+        else:
+            u = l[movie_id] 
+            u.append(s[:-1])
+            l[movie_id] = u
+    return l
 
 def netflix_print (w, i, j, k) :
     """
@@ -30,10 +47,18 @@ def netflix_solve (r, w) :
     r a reader
     w a writer
     """
+    usr_avg_cache = json.load(open('/u/dameng/CS373-test/netflix-tests/pma459-usrAvgCache.json', 'r'))
+    mov_avg_cache = json.load(open('/u/dameng/CS373-test/netflix-tests/pma459-mvAvgCache.json', 'r'))
+    probe_dict = probe_read(open('/u/dameng/CS373/probe.txt', 'r'))
+    print(usr_avg_cache['7'])
+    print(mov_avg_cache[2])
+    print(probe_dict['10001'])
+    
+    """
     for s in r :
         i, j, k = netflix_read(s)
         netflix_print(w, i, j, k)
-
+    """
 
 
 if __name__ == "__main__" :
